b51caa94ff034328d6c348871f07420d08c15fa3
Ainsey11 <ainsey11@gmail.com>
2018-09-18 17:52:46 +0100

    Added privmsg responder

--- RunBot.js
+++ RunBot.js
@@ -12,7 +12,7 @@ var greetmsg = config.IRC.GreetMsg;
 
 var client = new ircClient(server, port, myNick, fullname);
 client.verbosity = 2;
-
+client.debug = false;
 
 client.on('ready', function () {
     client.join(chan);
@@ -24,4 +24,10 @@ console.log("On port: " + port);
 console.log("As Nick: " + myNick);
 console.log("My real name is: " + fullname);
 console.log("I'm lurking in: " + chan);
+
+client.on('PRIVMSG', function (data) {
+    var message = 'Hi, ' + data.sender +', nice of you to speak to a bot. I can only repeat what you said: ' + data.message;
+    if(data.sender !== myNick) client.say(data.sender, message);
+  });
+
 client.connect();
