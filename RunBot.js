ce2a406be7830cef5a521b403e5375867d2ae209
Ainsey11 <ainsey11@users.noreply.github.com>
2018-09-18 20:31:49 +0100

    Added some comments, also added a trigger char for "commands"

--- RunBot.js
+++ RunBot.js
@@ -9,6 +9,7 @@ var myNick = config.IRC.BotName;
 var fullname = config.IRC.RealName;
 var chan = config.IRC.Channel;
 var greetmsg = config.IRC.GreetMsg;
+var trigger = config.IRC.TriggerChar;
 
 var client = new ircClient(server, port, myNick, fullname);
 client.verbosity = 2;
@@ -19,11 +20,13 @@ client.on('ready', function () {
     client.say(chan, greetmsg)
 });
 
+// Handy to see if the config options have been read correctly.
 console.log("Connecting to: " + server);
 console.log("On port: " + port);
 console.log("As Nick: " + myNick);
 console.log("My real name is: " + fullname);
 console.log("I'm lurking in: " + chan);
+console.log("I will be triggered when " + trigger + "Is on the front of the message in the channel");
 
 client.on('PRIVMSG', function (data) {
     var message = 'Oh, ' + data.sender +'you tried to message me, well... I am afraid to say that I am a bot, not a sentient being. So I am just gonna do a typical bot thing and send you your message back. Here it is : ' + data.message;
@@ -37,4 +40,17 @@ client.on('KICK', function (data) {
     client.say(data.receiver, chanMessage);
 });
 
-client.connect();
+client.on('INVITE', function (data) {
+    var message = 'Thank you for your invite to ' + data.message + ', ' + data.sender + 'However my home is #Logsee, so I will stick there. Maybe try getting your own bot?';
+    client.say(data.message, message);
+  });
+
+client.on('CHANMSG', function (data) {
+    if (data.message.match(trigger+'ping')) {
+        client.say(chan, 'pong')
+    }
+});
+
+
+// Connect to irc
+  client.connect();
