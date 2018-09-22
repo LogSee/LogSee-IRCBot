e88177940d875f1e22312ccf9661aa4de7c77ad9
P0pzi <tburtun@gmail.com>
2018-09-22 01:25:10 +0100

    Scraping

--- test.js
+++ test.js
@@ -1,18 +1,28 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0',
     'Content-Type' : 'application/x-www-form-urlencoded'
  }
+
+
+ var url = "https://duckduckgo.com/html?q=vim";
  
- var url = "https://duckduckgo.com/?q=vim" ;
- 
- request({url:url, headers: headers}, function(error, response, html){
-    if (!error) {
-        //console.log(html);
-        var $ = cheerio.load(html);
-        $('#href').text();
-        console.log($);
+ request.post({url:url, headers: headers}, function(error, response, html){
+    if (html) {
+        var chero = cheerio.load(html);
+        var foundResult = false; // Stops the each() loop after first result;
+
+        chero('#links').children().each((index, result) => {
+            if (result.attribs.class && !result.attribs.class.includes('--ad') && !foundResult) {
+
+                //console.log(index, result);
+                var firstlink = chero(result).find('.result__a')[0].attribs.href
+                console.log(firstlink);
+
+                foundResult = true;
+            }
+        })
     }
 
  });
