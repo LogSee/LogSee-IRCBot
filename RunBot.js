162f1cfd6b8a0d83f8260998717e9906c411841f
P0pzi <tburtun@gmail.com>
2018-09-22 01:42:34 +0100

    Bugfixing...

--- RunBot.js
+++ RunBot.js
@@ -98,6 +98,7 @@ client.on('CHANMSG', function (data) {
         client.say(chan, `GODAMMIT, YOU'RE SO STUPID. HOW ARE YOU EVEN ALLOWED TO CODE!`);
     };
     if ([`${trigger}ddg`].some(x => msg.startsWith(x))) {
+        console.log('DDG FIRED');
         var headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0',
             'Content-Type' : 'application/x-www-form-urlencoded'
