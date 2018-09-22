d949ce21df92dc8989f94d063a84946aa20c93c7
Ainsey11 <ainsey11@users.noreply.github.com>
2018-09-22 01:37:25 +0100

    merging popzi's magic, and adding url concat

--- RunBot.js
+++ RunBot.js
@@ -97,7 +97,33 @@ client.on('CHANMSG', function (data) {
         client.say(chan, `GODAMMIT, YOU'RE SO STUPID. HOW ARE YOU EVEN ALLOWED TO CODE!`);
     };
     if ([`${trigger}ddg`].some(x => msg.startsWith(x))) {
-        
+        var request = require('request');
+        var cheerio = require('cheerio');
+        var headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0',
+            'Content-Type' : 'application/x-www-form-urlencoded'
+        }
+        var messageconcat = data.message.replace(' ','+');
+        var url = "https://duckduckgo.com/html?q="+messageconcat;
+ 
+        request.post({url:url, headers: headers}, function(error, response, html){
+            if (html) {
+                var chero = cheerio.load(html);
+                var foundResult = false; // Stops the each() loop after first result;
+
+                chero('#links').children().each((index, result) => {
+                if (result.attribs.class && !result.attribs.class.includes('--ad') && !foundResult) {
+
+                //console.log(index, result);
+                var firstlink = chero(result).find('.result__a')[0].attribs.href
+                var title = chero(result).find('.result__a').text().trim();
+                console.log(title, firstlink);
+                client.say(chan, title +' ' + firstlink);
+                foundResult = true;
+            }
+        })
+    }
+ });
     };
     // Fetches web page title element
     if (data.message.match(httpregex)) { // Screw this bit.
